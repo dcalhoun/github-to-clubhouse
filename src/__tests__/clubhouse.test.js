8f0402651227b0eba104e557493d1e5607c59cbd
David Calhoun <dpcalhoun@gmail.com>
2018-03-12 20:38:33 -0500

    [change] Return empty string from getIdFromBranch

--- src/__tests__/clubhouse.test.js
+++ src/__tests__/clubhouse.test.js
@@ -6,4 +6,10 @@ describe('getIdFromBranch', () => {
   it('returns the id', () => {
     expect(getIdFromBranch('feature/ch123/add-foo-bar')).toBe('123');
   });
+
+  describe('when no match', () => {
+    it('returns an empty string', () => {
+      expect(getIdFromBranch('feature/123/add-foo-bar')).toBe('');
+    });
+  });
 });
--- src/clubhouse.js
+++ src/clubhouse.js
@@ -1,1 +1,6 @@
-exports.getIdFromBranch = name => RegExp(/\/ch(\d+)\//, 'g').exec(name)[1];
+exports.getIdFromBranch = name => {
+  const match = RegExp(/\/ch(\d+)\//, 'g').exec(name);
+  return match !== null ? match[1] : '';
+};
+
+exports.updateStory = id => {};
