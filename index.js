364d029a6e0a9c8851ffdbc5665602991cf41a6b
David Calhoun <dpcalhoun@gmail.com>
2018-03-12 20:39:18 -0500

    [change] WIP; explore GitHub webhook payload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = (req, res) => 'Hello, World!';
+const clubhouse = require('./src/clubhouse');
+const { json } = require('micro');
+
+module.exports = async (req, res) => {
+  const body = await json(req);
+  const type = req.headers['x-github-event'];
+  if (body.sender && body.sender.login === 'dcalhoun') {
+    if (type === 'create' && body.ref_type === 'branch') {
+      return `>>>>> ${clubhouse.getIdFromBranch(body.ref)}`;
+    }
+  }
+};
