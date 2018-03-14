65cd18a1df0c0fa436da0723346ee727691030fa
David Calhoun <dpcalhoun@gmail.com>
2018-03-13 22:03:40 -0500

    [change] WIP; extract ID from parsed request

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const { json } = require('micro');
 const { parseRequest } = require('./src/github');
+const {
+  markStoryAwaitingCodeReview,
+  markStoryInQa,
+  markStoryStarted,
+} = require('../clubhouse');
 
 module.exports = async (req, res) => {
   const body = await json(req);
-  const event = parseRequest(req.headers['x-github-event'], body);
-  return `>>>>> ${event}`;
+  const { event, id } = parseRequest(req.headers['x-github-event'], body);
+
+  switch (event) {
+    case 'branch':
+      markStoryStarted(id);
+      break;
+    case 'pullRequestReviewRequested':
+      markStoryAwaitingCodeReview(id);
+      break;
+    case 'pullRequestMerged':
+      markStoryInQa(id);
+      break;
+    default:
+  }
 };
--- src/github.js
+++ src/github.js
@@ -1,11 +1,14 @@
 const acknowledgedUser = sender => sender && sender.login === 'dcalhoun';
 
+// TODO: Update parseRequest to return event and id
+// const id = getIdFromBranch(body.pull_request.head.ref);
 exports.parseRequest = (event, body = {}) => {
   switch (true) {
     case acknowledgedUser(body.sender) &&
       event === 'create' &&
       body.ref_type === 'branch':
       return 'branch';
+    // TODO: Rename to focus on review requested
     case acknowledgedUser(body.sender) &&
       event === 'pull_request' &&
       body.action === 'opened':
