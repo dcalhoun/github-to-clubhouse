dd93bd58c538b4e80f305f74d7a6df6d647d1531
David Calhoun <dpcalhoun@gmail.com>
2018-03-13 20:16:31 -0500

    [add] Set story to "in dev"

--- src/__tests__/clubhouse.test.js
+++ src/__tests__/clubhouse.test.js
@@ -1,6 +1,9 @@
 const clubhouse = require('../clubhouse');
+const fetch = require('node-fetch');
 
-const { getIdFromBranch } = clubhouse;
+const { getIdFromBranch, startStory } = clubhouse;
+
+jest.mock('node-fetch');
 
 describe('getIdFromBranch', () => {
   it('returns the id', () => {
@@ -11,5 +14,18 @@ describe('getIdFromBranch', () => {
     it('returns an empty string', () => {
       expect(getIdFromBranch('feature/123/add-foo-bar')).toBe('');
     });
+  });
+});
+
+describe('startStory', () => {
+  it('sets the IN_DEV workflow state ID', () => {
+    startStory('123');
+    expect(fetch).toHaveBeenCalledWith(
+      'https://api.clubhouse.io/api/v2/stories/123',
+      {
+        method: 'PUT',
+        workflow_state_id: 500000007,
+      }
+    );
   });
 });
--- src/clubhouse.js
+++ src/clubhouse.js
@@ -1,6 +1,17 @@
+const fetch = require('node-fetch');
+
+const API = 'https://api.clubhouse.io/api/v2/stories';
+
+const IN_DEV = 500000007;
+const AWAITING_CODE_REVIEW = 500000030;
+const IN_QA = 500000016;
+
+const updateStory = (id, body) =>
+  fetch(`${API}/${id}`, { method: 'PUT', ...body });
+
 exports.getIdFromBranch = name => {
   const match = RegExp(/\/ch(\d+)\//, 'g').exec(name);
   return match !== null ? match[1] : '';
 };
 
-exports.updateStory = id => {};
+exports.startStory = id => updateStory(id, { workflow_state_id: IN_DEV });
